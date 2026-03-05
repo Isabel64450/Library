04ffd89e4b6e4630afe39a362e6b3ad4a5333055
isabel64450 <isa.dumas64@gmail.com>
2026-03-05 16:08:51 +0100

    add nav bar

--- src/Controller/LoanController.php
+++ src/Controller/LoanController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,4 +17,6 @@ final class LoanController extends AbstractController
             'controller_name' => 'LoanController',
         ]);
     }
+
+    
 }
